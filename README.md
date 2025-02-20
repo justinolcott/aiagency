3c6ca0ae959769e362e7254b66a7e058471a32d4
Justin Olcott <justinolcott@hotmail.com>
2025-02-20 14:29:08 -0700

    Add (almost) working example

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # aiagency
 
 Repo for CS 673 Computation Creativity
+
+
+
+
+# Help
+- Needed this for NV-Embed-v2
+```
+pip uninstall -y transformer-engine
+pip install torch==2.2.0
+pip install transformers==4.42.4
+pip install flash-attn==2.2.0
+pip install sentence-transformers==2.7.0
+```
+
+- install requirements.txt and then run `playwright install`
--- src/aiagency.py
+++ src/aiagency.py
@@ -1,45 +1,74 @@
+
+
 from dotenv import load_dotenv
 load_dotenv()
 
 from pydantic import BaseModel, Field
 
-# from langchain_openai import ChatOpenAI
+from langchain_openai import ChatOpenAI
 
-# from langgraph_supervisor import create_supervisor
-# from langgraph.prebuilt import create_react_agent
+from langgraph_supervisor import create_supervisor
+from langgraph.prebuilt import create_react_agent
 
-# from langgraph.checkpoint.memory import InMemorySaver
-# from langgraph.store.memory import InMemoryStore
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.store.memory import InMemoryStore
 
-# model = ChatOpenAI(model="gpt-4o")
+model = ChatOpenAI(model="gpt-4o-mini")
 
-# # Create specialized agents
 
-# def add(a: float, b: float) -> float:
-#     """Add two numbers."""
-#     return a + b
+# Tools
+# from git import Repo
+import os
+# def create_repo(project_name: str) -> None:
+#     """
+#     Create a new git repository under agent_workspace.
+#     """
+#     repo_path = f"agent_workspace/{project_name}"
+#     Repo.init(repo_path)
+#     return None
 
-# def multiply(a: float, b: float) -> float:
-#     """Multiply two numbers."""
-#     return a * b
+def make_dir(directory_path: str) -> None:
+    """
+    Create a new directory under agent_workspace for example:
+        "example_project" makes a new directory "agent_workspace/example_project"
+        "example_project/backend" makes a new directory "agent_workspace/example_project/backend"
+    """
+    print(f"Creating directory {directory_path}")
+    os.makedirs(f'agent_workspace/{directory_path}', exist_ok=True)
+    return None
 
-# def web_search(query: str) -> str:
-#     """Search the web for information."""
-#     return (
-#         "Here are the headcounts for each of the FAANG companies in 2024:\n"
-#         "1. **Facebook (Meta)**: 67,317 employees.\n"
-#         "2. **Apple**: 164,000 employees.\n"
-#         "3. **Amazon**: 1,551,000 employees.\n"
-#         "4. **Netflix**: 14,000 employees.\n"
-#         "5. **Google (Alphabet)**: 181,269 employees."
-#     )
+def make_file(file_path: str, content: str) -> None:
+    """
+    Create a new file under agent_workspace for example:
+        "example_project/backend/app.py" makes a new file "agent_workspace/example_project/backend/app.py"
+        "example_project/frontend/index.html" makes a new file "agent_workspace/example_project/frontend/index.html"
+    """
+    print(f"Creating file {file_path}")
+    with open(f'agent_workspace/{file_path}', "w") as f:
+        f.write(content)
+    return None
 
-# math_agent = create_react_agent(
-#     model=model,
-#     tools=[add, multiply],
-#     name="math_expert",
-#     prompt="You are a math expert. Always use one tool at a time."
-# )
+backend_swe_agent = create_react_agent(
+    model=model,
+    tools=[make_dir, make_file],
+    name="backend_swe",
+    prompt="You are a backend software engineer. You can create files and directories to build the project."
+           "You first need to make a backend directory in the project folder."
+           "Then you need to create all necessary files and content the backend directory."
+           "Return a summary of what was done and how to run the code to the supervisor."
+           "You have access to the following tools: make_dir, make_file."
+)
+
+frontend_swe_agent = create_react_agent(
+    model=model,
+    tools=[make_dir, make_file],
+    name="frontend_swe",
+    prompt="You are a frontend software engineer. You can create files and directories to build the project."
+              "You first need to make a frontend directory in the project folder."
+                "Then you need to create all necessary files and content the frontend directory."
+                "Return a summary of what was done and how to run the code to the supervisor."
+                "You have access to the following tools: make_dir, make_file."
+)
 
 # research_agent = create_react_agent(
 #     model=model,
@@ -48,7 +77,7 @@ from pydantic import BaseModel, Field
 #     prompt="You are a world class researcher with access to web search. Do not do any math."
 # )
 
-# # Create supervisor workflow
+# Create supervisor workflow
 # workflow = create_supervisor(
 #     [research_agent, math_agent],
 #     model=model,
@@ -59,21 +88,34 @@ from pydantic import BaseModel, Field
 #     )
 # )
 
+workflow = create_supervisor(
+    [backend_swe_agent, frontend_swe_agent],
+    tools=[make_dir, make_file],
+    model=model,
+    prompt=(
+        "You are a team supervisor managing two software engineers. "
+        "You first make a new directory for the project by using make_dir."
+        "Determine the interface between backend and frontend and assign tasks to each of them. Provide with the tasks they need and the name of the project_directory."
+        "They will then report back to you and you will return a summary of the entire project and instructions on how to run it."
+    )
+)
+
 # checkpointer = InMemorySaver()
 # store = InMemoryStore()
 
-# # Compile and run
-# app = workflow.compile(
-#     checkpointer=checkpointer,
-#     store=store
-# )
-# result = app.invoke({
-#     "messages": [
-#         {
-#             "role": "user",
-#             "content": "what's the combined headcount of the FAANG companies in 2024?"
-#         }
-#     ]
-# })
+# Compile and run
+app = workflow.compile(
+    # checkpointer=checkpointer,
+    # store=store
+)
+result = app.invoke({
+    "messages": [
+        {
+            "role": "user",
+            "content": "Make a frontend and backend for a todo app using python."
+        }
+    ]
+})
 
-# print(result)
+print(result)
+
